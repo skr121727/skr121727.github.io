443b69ce00a8d172591fce500d7f25c1c56fd084
skr121727 <skrizmo23@gmail.com>
2023-01-29 00:38:46 +0530

    change prices

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 (function () {
     const sarees = [
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-1.jpg", "price": "1050" },
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-2.jpg", "price": "1050" },
-        { "name": "Linen Cotton", "desc": "Green and Jerry Border", "iurl": "img/1-3.jpg", "price": "1050" },
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-4.jpg", "price": "1050" },
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/2-1.jpg", "price": "1590" },
-        { "name": "Chappa Silk", "desc": "Green and Jerry Border", "iurl": "img/2-2.jpg", "price": "1590" },
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-1.jpg", "price": "650" },
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-2.jpg", "price": "650" },
-        { "name": "Digital Print", "desc": "Green and Jerry Border", "iurl": "img/3-3.jpg", "price": "650" },
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-4.jpg", "price": "650" },
-        { "name": "Organja Butta", "desc": "Green and Jerry Border", "iurl": "img/3-5.jpg", "price": "650" },
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-6.jpg", "price": "650" },
-        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/4-1.jpg", "price": "350" },
-        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-1.jpg", "price": "750" },
-        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-2.jpg", "price": "750" },
-        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-3.jpg", "price": "750" },
-        { "name": "Softy Silk", "desc": "Green and Jerry Border", "iurl": "img/6-1.jpg", "price": "850" },
-        { "name": "Softy Silk", "desc": "Green and Jerry Border", "iurl": "img/6-2.jpg", "price": "850" }
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-1.jpg", "price": "1500" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-2.jpg", "price": "1500" },
+        { "name": "Linen Silk", "desc": "Green and Jerry Border", "iurl": "img/1-3.jpg", "price": "1600" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-4.jpg", "price": "1500" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/2-1.jpg", "price": "2300" },
+        { "name": "Chappa Silk", "desc": "Green and Jerry Border", "iurl": "img/2-2.jpg", "price": "2300" },
+        { "name": "Tussar Embose", "desc": "Orange melody", "iurl": "img/3-1.jpg", "price": "700" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-2.jpg", "price": "1100" },
+        { "name": "Digital Print", "desc": "Green and Jerry Border", "iurl": "img/3-3.jpg", "price": "1000" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-4.jpg", "price": "1000" },
+        { "name": "Organja Butta", "desc": "Green and Jerry Border", "iurl": "img/3-5.jpg", "price": "1200" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-6.jpg", "price": "1000" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/4-1.jpg", "price": "500" },
+        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-1.jpg", "price": "1250" },
+        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-2.jpg", "price": "1250" },
+        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-3.jpg", "price": "1250" },
+        { "name": "Softy Silk", "desc": "Green and Jerry Border", "iurl": "img/6-1.jpg", "price": "1500" },
+        { "name": "Softy Silk", "desc": "Green and Jerry Border", "iurl": "img/6-2.jpg", "price": "1500" }
     ];
 
     setTimeout(() => {
