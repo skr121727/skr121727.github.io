cfd2b1aad456e56c20e9c95fc6bf788dc0390042
skr121727 <skrizmo23@gmail.com>
2023-01-28 23:18:41 +0530

    fix grid errors

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 (function () {
     const sarees = [
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/1-1.jpg", "price": "1050" },
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/1-2.jpg", "price": "1050" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-1.jpg", "price": "1050" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-2.jpg", "price": "1050" },
         { "name": "Linen Cotton", "desc": "Green and Jerry Border", "iurl": "img/1-3.jpg", "price": "1050" },
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/1-4.jpg", "price": "1050" },
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/2-1.jpg", "price": "1590" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/1-4.jpg", "price": "1050" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/2-1.jpg", "price": "1590" },
         { "name": "Chappa Silk", "desc": "Green and Jerry Border", "iurl": "img/2-2.jpg", "price": "1590" },
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/3-1.jpg", "price": "650" },
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/3-2.jpg", "price": "650" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-1.jpg", "price": "650" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-2.jpg", "price": "650" },
         { "name": "Digital Print", "desc": "Green and Jerry Border", "iurl": "img/3-3.jpg", "price": "650" },
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/3-4.jpg", "price": "650" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-4.jpg", "price": "650" },
         { "name": "Organja Butta", "desc": "Green and Jerry Border", "iurl": "img/3-5.jpg", "price": "650" },
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/3-6.jpg", "price": "650" },
-        { "name": "Tusser Embose", "desc": "Green and Jerry Border", "iurl": "img/4-1.jpg", "price": "350" },
-        { "name": "Tusser Digital", "desc": "Green and Jerry Border", "iurl": "img/5-1.jpg", "price": "750" },
-        { "name": "Tusser Digital", "desc": "Green and Jerry Border", "iurl": "img/5-2.jpg", "price": "750" },
-        { "name": "Tusser Digital", "desc": "Green and Jerry Border", "iurl": "img/5-3.jpg", "price": "750" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/3-6.jpg", "price": "650" },
+        { "name": "Tussar Embose", "desc": "Green and Jerry Border", "iurl": "img/4-1.jpg", "price": "350" },
+        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-1.jpg", "price": "750" },
+        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-2.jpg", "price": "750" },
+        { "name": "Tussar Digital", "desc": "Green and Jerry Border", "iurl": "img/5-3.jpg", "price": "750" },
         { "name": "Softy Silk", "desc": "Green and Jerry Border", "iurl": "img/6-1.jpg", "price": "850" },
         { "name": "Softy Silk", "desc": "Green and Jerry Border", "iurl": "img/6-2.jpg", "price": "850" }
     ];
@@ -26,16 +26,18 @@
                 <div class="card" style="width: 18rem;">
                 <img src="${item.iurl}" class="card-img-top" alt="${item.name}">
                 <div class="card-body">
-                    <h5 class="card-title">${item.name} s</h5>
+                    <h5 class="card-title">${item.name}</h5>
                     <p class="card-text">${item.desc}</p>
-                    <p class="card-text">&#8360;&nbsp;${item.price}</p>
+                    <p class="card-text" data-price="${item.price}">&#8360;&nbsp;${item.price}</p>
                 </div>
                 </div>
             </div>`;
             console.log(item);
-            document.querySelector(".row").insertAdjacentHTML('afterend', _html);
+            document.querySelector(".row").insertAdjacentHTML('beforeend', _html);
         }
     });
+
+    
 
 })();
 
